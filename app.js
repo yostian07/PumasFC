4bf00ccd591dc4f1833b58ef09f4da22e337256e
Yostian <yostiancampos12@gmail.com>
2024-06-18 02:15:41 -0600

    configuracion db2

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ const pool = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 app.use(cors());
